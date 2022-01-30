ebf98d26bc5056bae7d069fecb130b36bb65d194
raghuldavidmrd333 <98683176+raghuldavidmrd333@users.noreply.github.com>
2022-01-30 12:54:33 +0530

    Add files via upload

---
+++ expense_tracker_app (2)/expense_tracker_app/src/main/java/com/RAGHUL/springexpensetracker/repos/BalanceRepository.java
@@ -0,0 +1,9 @@
+package com.RAGHUL.springexpensetracker.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sathish.springexpensetracker.models.Balance;
+
+public interface BalanceRepository extends JpaRepository<Balance, Long> {
+
+}
